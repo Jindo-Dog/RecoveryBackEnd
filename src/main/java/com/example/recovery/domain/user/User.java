e14ccd374d7ad36f66e590d38858a5d63debeab4
dragonray <dragonray@naver.com>
2026-03-28 18:54:19 +0900

    #2 Feat Table Domain 추가

---
+++ src/main/java/com/example/recovery/domain/user/User.java
@@ -0,0 +1,30 @@
+package com.example.recovery.domain.user;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "USER")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Long id;
+
+    @Column(name = "nickname", nullable = false, length = 40)
+    private String nickname;
+
+    @Column(name = "profile_url", length = 255)
+    private String profileUrl;
+
+    @ColumnDefault("now()")
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime createdAt;
+
+    @ColumnDefault("true")
+    @Column(name = "activation", nullable = false)
+    private Boolean activation;
+
+}
