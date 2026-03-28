107afb9337dc40637e7a3eba04760e30ee469756
dragonray <dragonray@naver.com>
2026-03-28 15:09:49 +0900

    #2 Refactor java25애서 불필요하게된 public 제어자 삭제

--- src/main/java/com/example/recovery/RecoveryApplication.java
+++ src/main/java/com/example/recovery/RecoveryApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecoveryApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(RecoveryApplication.class, args);
     }
 
