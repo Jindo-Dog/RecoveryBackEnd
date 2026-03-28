026ae5feac756a19ccbc97ebd6b499abcbcc2197
dragonray <dragonray@naver.com>
2026-03-28 14:57:29 +0900

    #2 Config 줄 구분 방식 변경 및 의존성 추가

--- src/main/java/com/example/recovery/RecoveryApplication.java
+++ src/main/java/com/example/recovery/RecoveryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecoveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecoveryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RecoveryApplication.class, args);
+    }
 
 }
