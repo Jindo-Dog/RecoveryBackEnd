749bfba39f16aaa132ed4f438ea1c5619803adea
SeungHyun Hwang <dragonray@naver.com>
2026-04-25 12:53:53 +0900

    #19 Feat 데이터 검증 추가

--- src/main/java/com/example/recovery/controller/memoirs/MemoirController.java
+++ src/main/java/com/example/recovery/controller/memoirs/MemoirController.java
@@ -4,6 +4,7 @@ import com.example.recovery.request.MemoirListRequest;
 import com.example.recovery.response.MemoirSimpleResponse;
 import com.example.recovery.service.memoirs.MemoirService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class MemoirController {
     private final MemoirService memoirService;
 
     @GetMapping()
-    public MemoirSimpleResponse viewMemoirsByList(MemoirListRequest request) {
+    public MemoirSimpleResponse viewMemoirsByList(@Validated MemoirListRequest request) {
         return memoirService.memoirList(request);
     }
 }
